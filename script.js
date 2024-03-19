497112a191c973c22ad71ce0455edb6bb2a5c33c
Glitch (glitch-hello-website) <none>
2024-03-19 04:45:46 +0000

    🎷👁 Checkpoint ./script.js:66041982/413 ./index.html:66041982/56

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
+let correcta_1= [1]
+let correcta_2= [1]
+let correcta_3= [3]
+
+let opcion_elegida_1=[]
+let opcion_elegida_2=[]
+let opcion_elegida_3=[]
+
+
+
 function Responder_1(){
   
-  let res
+  for (i=0;i<correcta_1.length; i++){
+    if (correcta_1[i]==opcion_elegida_1[i]){
+      document.print('La respuesta correcta es')
+    }
+  }
+    
 }
