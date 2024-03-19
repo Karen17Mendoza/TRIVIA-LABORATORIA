fd3159a39e777f02712672021042d09442f72b46
Glitch (glitch-hello-website) <none>
2024-03-19 05:08:41 +0000

    💎🏮 Checkpoint ./script.js:66041982/134 ./index.html:66041982/419

--- script.js
+++ script.js
@@ -5,10 +5,10 @@ let correcta_3= [3]
 let opcion_elegida_1=[]
 let opcion_elegida_2=[]
 let opcion_elegida_3=[]
-
+var x=0;
 
 
 function responder_1(){
-  
-    document.getElementById(resultado).innerHTML = La respuesta correcta es: Aritoteles
+  x=1;
+  document.getElementById("resultado").innerHTML = "la respuesta correcta es: "
 }
