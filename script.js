d09240ef5116567dfb287e56da6f385d4b56bea7
Glitch (glitch-hello-website) <none>
2024-03-20 21:19:14 +0000

    👏🍲 Checkpoint ./script.js:66041982/5886 ./index.html:66041982/13379

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 function iniciarJuego() {
-    // Ocultar la pantalla de bienvenida
-    document.getElementById("bienvenida").style.display = "none";
-    // Mostrar la pantalla de preguntas
-    document.getElementById("preguntas").style.display = "block";
-      var nombre = document.getElementById("nombre").value;
+    var nombre = document.getElementById("nombre").value;
     document.getElementById("mensajeBienvenida").textContent = "Hola " + nombre + ", bienvenido/a!";
     document.getElementById("bienvenida").style.display = "none";
     document.getElementById("preguntas").style.display = "block";
 }
+
+let respuestasCorrectas = 0;
+let puntaje = respuestasCorrectas*100;
 
 function verificarRespuesta(preguntaId) {
     // 
@@ -40,6 +39,7 @@ function verificarRespuesta(preguntaId) {
 
         // Comparar respuesta seleccionada con la respuesta correcta
         if (respuesta === respuestaCorrecta) {
+            respuestasCorrectas++;
             mensajeElemento.textContent = "¡Respuesta correcta!";
         } else {
             //mensajeElemento.textContent = "Respuesta incorrecta. La respuesta correcta es: " + respuestaCorrecta.toUpperCase();
@@ -49,21 +49,32 @@ function verificarRespuesta(preguntaId) {
     } else {
         mensajeElemento.textContent = "Por favor, seleccione una respuesta.";
     }
+  
+  document.getElementById("contador-respuestas").textContent = "Respuestas correctas: " + respuestasCorrectas;
+  document.getElementById("puntaje").textContent = "Tu puntaje total es: " + puntaje;
 }
 function reiniciarJuego() {
-    // Mostrar la sección de bienvenida y ocultar la sección de preguntas
+    // Reinicia el contador de respuestas correctas a cero
+    respuestasCorrectas = 0;
+    
+    // Actualiza el contenido del contador de respuestas en la pantalla
+    document.getElementById("contador-respuestas").textContent = "Respuestas correctas: " + respuestasCorrectas;  
+    
+  // Mostrar la sección de bienvenida y ocultar la sección de preguntas
+ 
     document.getElementById("bienvenida").style.display = "block";
     document.getElementById("preguntas").style.display = "none";
-
+    
+  
     // Reiniciar el contenido de los mensajes de respuesta
     document.getElementById("mensaje1").textContent = "";
     document.getElementById("mensaje2").textContent = "";
     document.getElementById("mensaje3").textContent = "";
-
+  
     // Limpiar las selecciones de radio buttons
     var opciones = document.querySelectorAll('input[type="radio"]');
     opciones.forEach(function(opcion) {
         opcion.checked = false;
-  
+   
     });
 }
